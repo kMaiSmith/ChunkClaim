81464e1898f3f8e45550de6f136cf27ea2dcb731
Kyle Smith <kmaismith@gmail.com>
2014-02-28 17:46:10 -0500

    Renamed PlayerHandlerTest to PlayerBlockHandlerTest for the sake of sanity

--- test/com/kmaismith/ChunkClaim/PlayerBlockHandlerTest.java
+++ test/com/kmaismith/ChunkClaim/PlayerBlockHandlerTest.java
@@ -17,7 +17,7 @@ import org.junit.Test;
 
 import static org.mockito.Mockito.*;
 
-public class PlayerHandlerTest {
+public class PlayerBlockHandlerTest {
 
     private PlayerEventHandler systemUnderTest;
     private DataManager mockDataStore;
