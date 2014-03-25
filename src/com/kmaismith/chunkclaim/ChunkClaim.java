7c97fb92928b53db0daaf7f8da6f5246b7c1ace0
Reloque <Reloque@gmail.com>
2014-03-25 13:42:33 -0400

    Re-reverted

--- src/com/kmaismith/chunkclaim/ChunkClaim.java
+++ src/com/kmaismith/chunkclaim/ChunkClaim.java
@@ -1,24 +1,25 @@
 /*
-ChunkClaim Plugin for Minecraft Bukkit Servers
-Copyright (C) 2012 Felix Schmidt
-Based on code by Felix Schmidt, Copyright (C) 2014 Kyle Smith
-This file is part of ChunkClaim.
+    ChunkClaim Plugin for Minecraft Bukkit Servers
+    Copyright (C) 2012 Felix Schmidt
+    Based on code by Felix Schmidt, Copyright (C) 2014 Kyle Smith
+    
+    This file is part of ChunkClaim.
 
-ChunkClaim is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
+    ChunkClaim is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
 
-ChunkClaim is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-GNU General Public License for more details.
+    ChunkClaim is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
 
-You should have received a copy of the GNU General Public License
-along with ChunkClaim. If not, see <http://www.gnu.org/licenses/>.
+    You should have received a copy of the GNU General Public License
+    along with ChunkClaim.  If not, see <http://www.gnu.org/licenses/>.
 
-Contact: Kyle Smith <kMaiSmith@gmail.com>
-*/
+    Contact: Kyle Smith <kMaiSmith@gmail.com>
+ */
 
 package com.kmaismith.chunkclaim;
 
